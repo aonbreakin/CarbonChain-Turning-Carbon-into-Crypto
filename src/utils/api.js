921aea09a35b9ee7aade213a3086abab28c806af
Aonbreakin Soft <soul0009@yahoo.com>
2025-10-29 21:50:26 +0700

    Delete src/utils directory

--- src/utils/api.js
+++
@@ -1,6 +0,0 @@
-export const fetchSensorData = async () => {
-  return [
-    { id: 1, carbonLevel: 420, energyGenerated: 5.2, timestamp: Date.now() },
-    { id: 2, carbonLevel: 390, energyGenerated: 6.1, timestamp: Date.now() },
-  ];
-};
