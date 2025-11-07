89a84a093610cffc1a09cad4e3921ff726614378
Aonbreakin Soft <soul0009@yahoo.com>
2025-11-08 04:48:43 +0700

    Modify App.jsx
    Change to direct access <Carbonchain /> without route

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import { useState } from 'react';
 //import viteLogo from '/vite.svg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
-import Carbonchain from './components/carbonchain-dapp.tsx';
-//import Carbonchain from './components/remixed-99c53481.tsx';
-
+import Carbonchain from './components/carbonchain-dapp';
+//import ConnectWalletAdvanced from './components/ConnectWalletAdvanced.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /*const [count, setCount] = useState(0)*/
+  const [connected, setConnected] = useState(false);
   return (
     <>
-      
+	<Carbonchain  />
+	{/*!connected? (<Carbonchain />):(<ConnectWalletAdvanced  />)*/}
+      {/*<Link to="/"></Link>
 	  
-	  {}
-      <Link to="/"></Link>
 	  <Routes>
-	  <Route path="/" element={<Carbonchain />} />
+	  <Route path="/" element={<ConnectWalletAdvanced />} />
+		<Route path="/carbonchain-dapp" element={<Carbonchain />} />*/}
+	  
 		{/* Catch-all route for 404 Not Found */}
 			{/*<Route path="*" element={<NotFound />} />*/}
-	</Routes>
+			
+			{/*</Routes>*/}
 	  
     </>
   )
